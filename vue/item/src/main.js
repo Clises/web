a2f6918a19133c6150480ef62efe57fe1c8f3c23
EDZ <chenglin.gao@plusx.cn>
2018-06-21 10:48:14 +0800

    no message

---
+++ vue/item/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+// import App from './App.vue'
+import procedure from './procedure'
+
+
+
+
+
+Vue.component("procedure",procedure)
+var myVue=new Vue({
+  el:'#app',
+  data:{
+
+  }
+})
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// })
---
+++ vue/item/src/procedure.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data:function () {
+          return{
+            msg:"hello"
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
