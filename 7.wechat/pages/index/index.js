ad6689e5a9eb605e038e7a6a196945f001ebb9c7
EDZ <chenglin.gao@plusx.cn>
2018-07-19 18:01:04 +0800

    ······

--- 7.wechat/pages/index/index.js
+++ 7.wechat/pages/index/index.js
@@ -1,54 +1,58 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+let bmap = require('../../lib/bmap-wx.js')
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    //获取当前天气数据
+    init(params) {
+        let that = this
+        let BMap = new bmap.BMapWX({
+            ak: globalData.ak,
+        })
+        BMap.weather({
+            location: params.location,
+            fail: that.fail,
+            success: that.success,
+        })
+    },
+    //获取地理位置
+    fail(res) {
+      wx.stopPullDownRefresh()
+      let errMsg = res.errMsg || ''
+      // 拒绝授权地理位置权限
+      if (errMsg.indexOf('deny') !== -1 || errMsg.indexOf('denied') !== -1) {
+        wx.showToast({
+          title: '需要开启地理位置权限',
+          icon: 'none',
+          duration: 3000,
+          success(res) {
+            let timer = setTimeout(() => {
+              clearTimeout(timer)
+              wx.openSetting({})
+            }, 3000)
+          },
+        })
+      } else {
+        wx.showToast({
+          title: '网络不给力，请稍后再试',
+          icon: 'none',
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+    },
+    //地理位置获取成功之后，执行 success
+    success (data) {
+        wx.stopPullDownRefresh()
+        let now = new Date()
+        // 存下来源数据
+        data.updateTime = now.getTime()
+        data.updateTimeFormat = utils.formatDate(now, "MM-dd hh:mm")
+        let results = data.originalData.results[0] || {}
+        data.pm = this.calcPM(results['pm25'])
+        // 当天实时温度
+        data.temperature = `${results.weather_data[0].date.match(/\d+/g)[2]}`
+        wx.setStorage({
+            key: 'cityDatas',
+            data: data,
+        })
+        this.setData({
+            cityDatas: data,
+        })
+    },
 })
